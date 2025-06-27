88fffa0149bc3b42477c610a998c191abf180170
srijanid246 <srijani246@gmail.com>
2025-06-27 10:56:56 +0530

    upload file changes

---
+++ migrate.py
@@ -0,0 +1,7 @@
+#from app import app, db
+#from sqlalchemy import text
+
+#with app.app_context():
+  #  with db.engine.connect() as conn:
+   #     conn.execute(text('ALTER TABLE todo ADD COLUMN "order" INTEGER DEFAULT 0;'))
+   #     print("✅ Column 'order' added successfully!")
